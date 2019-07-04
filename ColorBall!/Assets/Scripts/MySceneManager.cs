17718551f2c418feac70152035df38c05a04a9dd
emirhansenkal <46650883+emirhansenkal@users.noreply.github.com>
2019-07-04 13:18:58 +0300

    Stage Manager

---
+++ ColorBall!/Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Emirhan's Scene");
+        }
+    }
+}
---
+++ ColorBall!/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoSingleton<ObjectManager>
+{
+    #region Variables
+    ObjectPoolManager objectPoolManager;
+    [Header("Random Variables")]
+    [SerializeField] int randomTypeValueUpperLimit = 3, randomTypeValueLowerLimit = 0;
+    [Header("Position Variables")]
+    [SerializeField] Vector3 nextPosition = Vector3.zero;
+    [SerializeField] Vector3 stepVector = new Vector3(0,0,5);
+
+    #endregion
+
+    #region Functions
+    private void Awake()
+    {
+        objectPoolManager = ObjectPoolManager.Instance;
+
+        nextPosition = stepVector;
+    }
+
+    public void spawnObjects(int numberOfObjects)
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            int randomType = Random.Range(randomTypeValueLowerLimit, randomTypeValueUpperLimit);
+            objectPoolManager.spawnObject(randomType, nextPosition);
+            nextPosition += stepVector;
+        }
+    }
+    #endregion
+}
