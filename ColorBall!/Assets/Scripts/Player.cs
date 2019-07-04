4f23d420811d732703aad73d4c52dd7f082bf233
hbkaraman <hakanburakkaraman@gmail.com>
2019-07-04 13:26:15 +0300

    DoTween&Itween Added

--- ColorBall!/Assets/Scripts/Player.cs
+++ ColorBall!/Assets/Scripts/Player.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody _rb;
+    public Vector3 target;
+    private bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        AddForceMovement();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isMoving);
+        MovementController();
+    }
+
+    private void MovementController()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            isMoving = true;
+            target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , 0.7f, Input.mousePosition.z / Screen.width));
+            Debug.Log(target);
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isMoving = false;
+        }
         
+    }
+
+    private void AddForceMovement()
+    {
+        if (isMoving)
+        {
+            _rb.MovePosition(target * speed * Time.deltaTime);
+        }
     }
 }
